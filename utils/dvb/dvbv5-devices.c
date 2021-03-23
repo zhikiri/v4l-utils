62d6de218621d598ec8d5f878193ee400ff2773c
zhikiri <zhikiri.dev@gmail.com>
2021-03-23 15:12:13 +0200

    added device check logic

--- utils/dvb/dvbv5-devices.c
+++ utils/dvb/dvbv5-devices.c
@@ -1,21 +1,45 @@
 #include <stdio.h>
+#include <config.h>
 #include <libdvbv5/dvb-dev.h>
+
+# define _(string) string
 
 int main() {
 
     struct dvb_device *dvb;
-    // struct dvb_dev_list *dvb_dev_list;
+    struct dvb_open_descriptor *dev_desc;
+    struct dvb_v5_fe_parms *parms;
+
+    struct dvb_dev_list device;
 
     dvb = dvb_dev_alloc();
     dvb_dev_find(dvb, NULL, NULL);
+    parms = dvb->fe_parms;
+
     if (dvb->num_devices == 0) {
-        printf("Cannot find any connected devices\n");
-        return 0;
+        dvb_log(_("There are no connected devices %d"), dvb->num_devices);
+        return 1;
     }
 
+    dvb_log(_("Found #%d connected devices"), dvb->num_devices);
+
     for (int i = 0; i < dvb->num_devices; i++) {
-        char *name = dvb->devices[i].sysname;
-        printf("DBG : Device sysname %s\n", name);
+        device = dvb->devices[i];
+        if (device.dvb_type != DVB_DEVICE_FRONTEND)  {
+            // Skip not frontend devices
+            continue;
+        }
+
+        // Trying to connect to the device frontend
+        dev_desc = dvb_dev_open(dvb, device.sysname, O_RDONLY);
+        if (!dev_desc) {
+            dvb_log(_("Device (path: %s) - FAIL"), device.path);
+            continue;
+        }
+
+        dvb_log(_("Device (path: %s) - OK"), device.path);
+        // Close device connection
+        dvb_dev_close(dev_desc);
     }
 
     // Clean memory
