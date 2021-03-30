d5972a6192d295179493a7c6146fd43c74d3d752
zhikiri <zhikiri.dev@gmail.com>
2021-03-30 12:34:33 +0300

    changed the dvb-devices output format

--- utils/dvb/dvbv5-devices.c
+++ utils/dvb/dvbv5-devices.c
@@ -2,26 +2,21 @@
 #include <config.h>
 #include <libdvbv5/dvb-dev.h>
 
-# define _(string) string
-
 int main() {
-
     struct dvb_device *dvb;
     struct dvb_open_descriptor *dev_desc;
-    struct dvb_v5_fe_parms *parms;
-
     struct dvb_dev_list device;
 
     dvb = dvb_dev_alloc();
     dvb_dev_find(dvb, NULL, NULL);
-    parms = dvb->fe_parms;
 
     if (dvb->num_devices == 0) {
-        dvb_log(_("There are no connected devices %d"), dvb->num_devices);
+        // Return a JSON with error `{"error": "There are not connected devices", "devices": {}}`
+        printf("{\"error\": \"There are no connected devices\", \"devices\": {}}");
         return 1;
     }
 
-    dvb_log(_("Found #%d connected devices"), dvb->num_devices);
+    printf("{\"error\": \"There are no connected devices\", \"devices\": {");
 
     for (int i = 0; i < dvb->num_devices; i++) {
         device = dvb->devices[i];
@@ -30,14 +25,23 @@ int main() {
             continue;
         }
 
+        // Prints `"/dev/dvb/adapterX/frontend0": `
+        printf("{\"%s\": ", device.path);
+
         // Trying to connect to the device frontend
         dev_desc = dvb_dev_open(dvb, device.sysname, O_RDONLY);
         if (!dev_desc) {
-            dvb_log(_("Device (path: %s) - FAIL"), device.path);
+            // Device cannot be open, mark it as FALSE
+            printf("true");
             continue;
         }
 
-        dvb_log(_("Device (path: %s) - OK"), device.path);
+        printf("false");
+
+        if (i != (dvb->num_devices - 1)) {
+            printf(",");
+        }
+
         // Close device connection
         dvb_dev_close(dev_desc);
     }
@@ -46,4 +50,3 @@ int main() {
     dvb_dev_free(dvb);
     return 0;
 }
-
